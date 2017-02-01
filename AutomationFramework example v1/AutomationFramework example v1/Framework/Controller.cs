767b3c6f2245eb80cf48992d419eb0dff9ae1e3c
ahaynes <ahaynes@300009015WS>
2017-02-01 12:49:40 -0600

    Began structuring framework classes and coding

---
+++ AutomationFramework example v1/AutomationFramework example v1/Framework/Controller.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomationFramework_example_v1
+{
+    class Controller
+    {
+        public Controller(string AutomationTestSuitePath)
+        {
+            //Read data storage information for automation test suites
+            //foreach test suite listed to execute
+            //{
+                //start a timer to record test suite execution time
+                //read test suite data for individual tests to execute
+                //foreach test
+                //{
+                    //start a timer to record test execution time
+                    //read associated project and environment information
+                    //read test data 
+                    //launch the project in environment
+                    //foreach test step
+                    //{
+                        //start a timer to record total step execution time
+                        //read test step data
+                        //read associated action data
+                        //read associated keyword data
+                        //read associated user information
+                        //start a timer to record command execution time
+                        //execute step command
+                        //stop command execution timer
+                        //stop total step execution timer
+                        //Log information using current timer ticks as current information
+                    //}
+                    //stop test execution timer
+                    //Log test execution time
+                //}
+                //stop test suite execution timer
+                //log test suite execution time
+            //}
+
+            //cleanup and exit framework
+        }
+        
+    }
+}
---
+++ AutomationFramework example v1/AutomationFramework example v1/Program.cs
@@ -0,0 +1,18 @@
+﻿
+
+namespace AutomationFramework_example_v1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //may be able to pass in the path for the test suite inventory to drive the current instance of the framework
+            //use args to set up framework properties like debugging and show console ect.
+
+            //call the appropriate controller method, this may also be controled by argument
+            new Controller("my//automation//test//suite.xlsx");
+
+            //the command line command structure to call the framework would need to be well documented
+        }
+    }
+}
