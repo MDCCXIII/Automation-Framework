aaff1501c5155f7300a59edf80c8ec8439bfc910
ahaynes <ahaynes@300009015WS>
2017-02-02 08:44:51 -0600

    refactor

--- AutomationFramework example v1/AutomationFramework example v1/Examples/MyFirstTest.cs
+++ AutomationFramework example v1/AutomationFramework example v1/Examples/MyFirstTest.cs
@@ -11,30 +11,36 @@ namespace AutomationFramework_example_v1
     class MyFirstTest
     {
         public static IWebDriver driver = new ChromeDriver();
-
-        static void Main(string[] args)
+        static void Main1()
         {
             driver.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(30));
             driver.Manage().Timeouts().SetPageLoadTimeout(TimeSpan.FromSeconds(30));
             driver.Manage().Window.Maximize();
+            // Navagation to URL
             driver.Url = "https://aarp-test.kanacloud.com/GTConnect/UnifiedAcceptor/AARPDesktop.Main";
+            // Login
             IWebElement Text_UserName = new Elements().ById("userName");
             Text_UserName.SendKeys("ALP_MRA_15");
             IWebElement Text_Password = new Elements().ById("password");
-            Text_Password.SendKeys("password1");
+            Text_Password.SendKeys("password2");
             IWebElement Button_Login = new Elements().ById("loginButton");
             Button_Login.Click();
             By btnContinue = By.Id("f5_btnContinue");
+            // Work around if another user is login
             if (driver.IsElementPresent(btnContinue))
             {
                 IWebElement Button_Continue = new Elements().ById(btnContinue);
                 Button_Continue.Click();
             }
+            driver.Wait(2);
+            // Navagation to Handle Call screen 
             IWebElement Link_Actionable_Dashboard = new Elements().ByXpath("//{0}[contains({1}, '{2}')] ", new string[] { "a", "text()", "Handle Call" });
             Link_Actionable_Dashboard.Click();
+            // Verify Page Header
             string pageXpath = "//div[contains(@id, 'tfhPerspectives')][contains(@style,'visibility: visible')]//div[contains(@id, 'fhdVerbRunner')]/div[contains(@id, 'innerForm')][not(contains(@style, 'display: none'))]//{0}[contains({1}, '{2}')]";
             IWebElement PageHeader = new Elements().ByXpath(pageXpath, new string[] { "h1", "@id", "lblIdentifyPerson" });
             if (!PageHeader.TextEqual("Identify Person")) { throw new Exception("Failed"); }
+            // Enter Identify Person search fields
             IWebElement LastNameText = new Elements().ByXpath(pageXpath, new string[] { "input", "@id", "txtLastName" });
             LastNameText.SendKeys("Smith");
             IWebElement FirstNameText = new Elements().ByXpath(pageXpath, new string[] { "input", "@id", "txtFirstName" });
@@ -47,7 +53,31 @@ namespace AutomationFramework_example_v1
             ViewProfileCheckbox.Click();
             IWebElement SearchButton = new Elements().ByXpath(pageXpath, new string[] { "button", "@id", "btnSearch" });
             SearchButton.Click();
+            // Locate the Results Table
             IWebElement ResultsTable = new Elements().ByXpath(pageXpath, new string[] { "div", "@id", "lstPerson_table" });
+            // Locate first entry in DOB column in Results Table 
+            IWebElement DobResultsTable = new Elements().ByXpath(pageXpath, new string[] { "a", "@id", "lstPerson_row0_col5" });
+            // Verify DOB = expected results
+            if (!DobResultsTable.TextEqual("07/01/1946")) { throw new Exception("Failed"); }
+            // Locate and click Wrapup button
+            IWebElement WrapupButton = new Elements().ByXpath("//button[contains(@id, 'btnWrapup') and not(contains(@id, 'info'))]");
+            WrapupButton.Click();
+            // Locate and select Disposition Select option X-Research
+            IWebElement DispositionSelect = new Elements().ByXpath("//div[contains(@id, 'tfhPerspectives')][contains(@style, 'visibility: visible')]//div[contains(@id, 'fhdVerbRunner')]/div[contains(@id, 'innerForm')][not(contains(@style, 'display: none'))]//select[contains(@id, 'optReasonCodes')]");
+            DispositionSelect.SelectOptionByText("X-Research");
+            // Locate and select YesNo Select option Yes
+            IWebElement YesNoSelect = new Elements().ByXpath("//div[contains(@id, 'tfhPerspectives')][contains(@style, 'visibility: visible')]//div[contains(@id, 'fhdVerbRunner')]/div[contains(@id, 'innerForm')][not(contains(@style, 'display: none'))]//select[contains(@id, 'optYesNo')]");
+            YesNoSelect.SelectOptionByText("Yes");
+            driver.Wait(2);
+            // Locate and click confirm button
+            IWebElement ConfirmButton = new Elements().ByXpath("//div[contains(@id, 'tfhPerspectives')][contains(@style, 'visibility: visible')]//div[contains(@id, 'fhdVerbRunner')]/div[contains(@id, 'innerForm')][not(contains(@style, 'display: none'))]//button[contains(@id, 'btnConfirm')]");
+            ConfirmButton.Click();
+            driver.Wait(2);
+            // Locate and click logout button
+            IWebElement LogoutButton = new Elements().ById("f7_btnLogout");
+            LogoutButton.Click();
+            IWebElement ConfirmLogoutButton = new Elements().ByXpath("//button[contains(@id, 'btnConfirm')]");
+            ConfirmLogoutButton.Click();
         }
     }
 
@@ -95,7 +125,8 @@ namespace AutomationFramework_example_v1
                         if (driver.IsElementPresent(by)) return driver.FindElement(by);
                         Thread.Sleep(1000);
                     }
-                } catch (NoSuchElementException)
+                }
+                catch (NoSuchElementException)
                 {
 
                 }
@@ -106,7 +137,7 @@ namespace AutomationFramework_example_v1
         public static bool TextContains(this IWebElement element, string text)
         {
             bool result = false;
-            if(element.Text.Contains(text))
+            if (element.Text.Contains(text))
             {
                 result = true;
             }
@@ -125,6 +156,11 @@ namespace AutomationFramework_example_v1
             return result;
         }
 
+        public static void SelectOptionByText(this IWebElement element, string optionText)
+        {
+            new SelectElement(element).SelectByText(optionText);
+        }
+
     }
-        
+
 }
