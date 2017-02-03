80f1b25725c057f43e9e5e10a0f6e3dd28d3e1e9
ahaynes <ahaynes@300009015WS>
2017-02-03 13:29:19 -0600

    Tested DAO, Needs more work

--- AutomationFramework example v1/AutomationFramework example v1/Program.cs
+++ AutomationFramework example v1/AutomationFramework example v1/Program.cs
@@ -1,5 +1,8 @@
 ﻿
 
+using AutomationFramework_example_v1.Framework.SQL;
+using System.Data;
+
 namespace AutomationFramework_example_v1
 {
     class Program
@@ -11,12 +14,16 @@ namespace AutomationFramework_example_v1
 
         static void Main(string[] args)
         {
+            Command cmd = new Command("getSuiteInformation");
+            DataSet ds = DAO.ExecuteStoredProcedure(cmd);
+            
             //MyFirstTest.Main1();
             //may be able to pass in the path for the test suite inventory to drive the current instance of the framework
             //use args to set up framework properties like debugging and show console ect.
 
             //call the appropriate controller method, this may also be controled by argument
             new Controller("my//automation//test//suite.xlsx");
+
 
             //the command line command structure to call the framework would need to be well documented
         }
