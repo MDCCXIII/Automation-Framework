675448893ab473aaf9c021c8232485c80e3cc23d
ahaynes <ahaynes@300009015WS>
2017-02-07 09:34:07 -0600

    Minor Cleanup

--- AutomationFramework example v1/AutomationFramework example v1/Framework/Extensions.cs
+++ AutomationFramework example v1/AutomationFramework example v1/Framework/Extensions.cs
@@ -49,6 +49,7 @@ namespace AutomationFramework_example_v1.Framework
         {
             return GetFieldName(() => field);
         }
+
         private static string GetFieldName<T>(Expression<Func<T>> expr)
         {
             var body = ((MemberExpression)expr.Body);
